8ca593014229ca40f048daeb2a0868900a074a38
t-kamiya-2424 <83796272+t-kamiya-2424@users.noreply.github.com>
2021-05-31 22:36:27 +0900

    Add files via upload

---
+++ fit/fit_pc.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+
+$(function(){
+// common
+    $(".txt").fitText(10);
+	$("h1").fitText(10);
+
+
+
+		
+});
---
+++ fit/fit_sp.js
@@ -0,0 +1,38 @@
+// JavaScript Document
+
+$(function(){
+// common
+    $(".txt").fitText(4);
+		$("h1").fitText(2.7);
+		
+		$(".h4").fitText(3);
+		
+	  // top
+		$("#news").fitText(3);
+		$("#news-tit").fitText(3);
+		$("#pro-txt01").fitText(2);
+		$("#pro-txt02").fitText(3);
+		$("#map-tit").fitText(2);
+		
+		
+		
+		// top
+		$(".com-tit01").fitText(2);
+		$(".com-tit02").fitText(1.8);
+		
+		
+		// good
+		$(".good01").fitText(2.5);
+		
+			// pr
+		$(".pr").fitText(2);
+		$(".pr-bg").fitText(2.5);
+			
+			//ac
+		$("ac-bg").fitText(2.5);
+		
+	
+
+
+  
+});
